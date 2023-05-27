7278ead84f01a9cb1e2c27b3e10f9d9a4e2910cd
Tanay Pant <tanaypant@protonmail.com>
2023-05-27 11:33:54 +0200

    Updates

--- bin/.local/bin/tmux-cht.sh
+++ bin/.local/bin/tmux-cht.sh
@@ -9,7 +9,7 @@ selected=$(echo -e "$languages\n$core_utils" | fzf)
 read -p "Your query: " query
 
 if echo "$languages" | grep -qs $selected; then
-	tmux split-window -h bash -c "curl cht.sh/$selected/$(echo "$query" | tr " " "+") | less"
+	tmux neww -n "$selected" bash -c "curl cht.sh/$selected/$(echo "$query" | tr " " "+") | less"
 else
-	tmux split-window -h bash -c "curl cht.sh/$selected~$query | less"
+	tmux neww -n "$selected" bash -c "curl cht.sh/$selected~$query | less"
 fi

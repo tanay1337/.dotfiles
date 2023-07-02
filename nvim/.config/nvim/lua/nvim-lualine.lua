86654594592c42ac316b7223cd945b569a45ff70
Tanay Pant <tanaypant@protonmail.com>
2023-07-02 10:55:31 +0200

    Customize Lualine

--- nvim/.config/nvim/lua/nvim-lualine.lua
+++ nvim/.config/nvim/lua/nvim-lualine.lua
@@ -2,21 +2,20 @@
 -- Author: shadmansaleh
 -- Credit: glepnir
 local lualine = require("lualine")
-
 -- Color table for highlights
 -- stylua: ignore
 local colors = {
-  -- bg       = '#202328',
-  fg       = '#bbc2cf',
-  yellow   = '#ECBE7B',
+  bg       = '#1e1e2d',
+  fg       = '#cdd6f4',
+  yellow   = '#f9e2af',
   cyan     = '#008080',
-  darkblue = '#081633',
-  green    = '#98be65',
-  orange   = '#FF8800',
-  violet   = '#a9a1e1',
-  magenta  = '#c678dd',
-  blue     = '#51afef',
-  red      = '#ec5f67',
+  darkblue = '#042761',
+  green    = '#a6e3a1',
+  orange   = '#fab387',
+  violet   = '#cba6f7',
+  magenta  = '#f49ac2',
+  blue     = '#89b4fa',
+  red      = '#f38ba8',
 }
 
 local conditions = {
@@ -39,17 +38,13 @@ local config = {
 		-- Disable sections and component separators
 		component_separators = "",
 		section_separators = "",
-		theme = {
-			-- We are going to use lualine_c an lualine_x as left and
-			-- right section. Both are highlighted by c theme .  So we
-			-- are just setting default looks o statusline
-			normal = { c = { fg = colors.fg, bg = colors.bg } },
-			inactive = { c = { fg = colors.fg, bg = colors.bg } },
-		},
+		theme = "onedark",
 	},
 	sections = {
 		-- these are to remove the defaults
-		lualine_a = {},
+		lualine_a = {
+			{ "mode", right_padding = 2 },
+		},
 		lualine_b = {},
 		lualine_y = {},
 		lualine_z = {},
@@ -77,54 +72,6 @@ end
 local function ins_right(component)
 	table.insert(config.sections.lualine_x, component)
 end
-
-ins_left({
-	function()
-		return "▊"
-	end,
-	color = { fg = colors.blue }, -- Sets highlighting of component
-	padding = { left = 0, right = 1 }, -- We don't need space before this
-})
-
-ins_left({
-	-- mode component
-	function()
-		return "👽"
-	end,
-	color = function()
-		-- auto change color according to neovims mode
-		local mode_color = {
-			n = colors.red,
-			i = colors.green,
-			v = colors.blue,
-			[""] = colors.blue,
-			V = colors.blue,
-			c = colors.magenta,
-			no = colors.red,
-			s = colors.orange,
-			S = colors.orange,
-			[""] = colors.orange,
-			ic = colors.yellow,
-			R = colors.violet,
-			Rv = colors.violet,
-			cv = colors.red,
-			ce = colors.red,
-			r = colors.cyan,
-			rm = colors.cyan,
-			["r?"] = colors.cyan,
-			["!"] = colors.red,
-			t = colors.red,
-		}
-		return { fg = mode_color[vim.fn.mode()] }
-	end,
-	padding = { right = 1 },
-})
-
-ins_left({
-	-- filesize component
-	"filesize",
-	cond = conditions.buffer_not_empty,
-})
 
 ins_left({
 	"filename",
@@ -155,7 +102,23 @@ ins_left({
 	end,
 })
 
-ins_left({
+-- Add components to right sections
+
+-- ins_right({
+-- 	"o:encoding", -- option component same as &encoding in viml
+-- 	fmt = string.upper, -- I'm not sure why it's upper case either ;)
+-- 	cond = conditions.hide_in_width,
+-- 	color = { fg = colors.green, gui = "bold" },
+-- })
+--
+-- ins_right({
+-- 	"fileformat",
+-- 	fmt = string.upper,
+-- 	icons_enabled = false, -- I think icons are cool but Eviline doesn't have them. sigh
+-- 	color = { fg = colors.green, gui = "bold" },
+-- })
+
+ins_right({
 	-- Lsp server name .
 	function()
 		local msg = "No Active Lsp"
@@ -172,22 +135,7 @@ ins_left({
 		end
 		return msg
 	end,
-	icon = " LSP:",
-	color = { fg = "#ffffff", gui = "bold" },
-})
-
--- Add components to right sections
-ins_right({
-	"o:encoding", -- option component same as &encoding in viml
-	fmt = string.upper, -- I'm not sure why it's upper case either ;)
-	cond = conditions.hide_in_width,
-	color = { fg = colors.green, gui = "bold" },
-})
-
-ins_right({
-	"fileformat",
-	fmt = string.upper,
-	icons_enabled = true, -- I think icons are cool but Eviline doesn't have them. sigh
+	icon = " ",
 	color = { fg = colors.green, gui = "bold" },
 })
 
@@ -200,21 +148,13 @@ ins_right({
 ins_right({
 	"diff",
 	-- Is it me or the symbol for modified us really weird
-	symbols = { added = " ", modified = "柳 ", removed = " " },
+	symbols = { added = " ", modified = "󰝤 ", removed = " " },
 	diff_color = {
 		added = { fg = colors.green },
 		modified = { fg = colors.orange },
 		removed = { fg = colors.red },
 	},
 	cond = conditions.hide_in_width,
-})
-
-ins_right({
-	function()
-		return "▊"
-	end,
-	color = { fg = colors.blue },
-	padding = { left = 1 },
 })
 
 -- Now don't forget to initialize lualine

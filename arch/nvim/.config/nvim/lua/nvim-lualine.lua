c4abd48e01502f9e375309b5ea4e0816e6f53f0a
Tanay Pant <tanaypant@protonmail.com>
2025-03-27 18:46:06 +0100

    install kdenlive, fix deprecated function for nvim config

--- arch/nvim/.config/nvim/lua/nvim-lualine.lua
+++ arch/nvim/.config/nvim/lua/nvim-lualine.lua
@@ -123,7 +123,7 @@ ins_right({
 	function()
 		local msg = "No Active LSP"
 		local buf_ft = vim.api.nvim_buf_get_option(0, "filetype")
-		local clients = vim.lsp.get_active_clients()
+		local clients = vim.lsp.get_clients()
 		if next(clients) == nil then
 			return msg
 		end
